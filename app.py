aeed02c56f45dcff18f6998da36f9f1bc55df5ba
Melops <95263124+Melonps@users.noreply.github.com>
2023-07-03 00:42:01 +0900

    [ADD] フォントなど必要なものを追加

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
-from drawsingraph import drawsingraph
-
+from lib.drawsingraph import drawsingraph
+from lib.drawnameimage import drawnameimage
+from PIL import Image
+import io
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # CORSを有効にする
 
 
 @app.route("/", methods=["GET"])
@@ -19,11 +21,35 @@ def hello_name(name):
 
 @app.route("/getsinimage", methods=["POST"])
 def gensinimage():
-    angle = request.json.get("angle")
+    angle = request.json.get("angle")  # POSTリクエストからangleパラメータを取得
     print(angle)
     print(type(angle))
-    drawsingraph(int(angle))
-    return send_file("./GeneratedData/sin.png", mimetype="image/png")
+    drawsingraph(int(angle))  # angleを整数に変換してdrawsingraph関数を呼び出す
+    return send_file("./GeneratedData/sin.png", mimetype="image/png")  # 生成された画像ファイルを送信
+
+
+@app.route("/getnameimage/<name>", methods=["POST"])
+def gennameimage(name):
+    drawnameimage(name)  # drawnameimage関数を呼び出す
+    return send_file("./GeneratedData/name.png", mimetype="image/png")  # 生成された画像ファイルを送信
+
+
+@app.route("/convertToMonochrome", methods=["POST"])
+def convert_to_monochrome():
+    if "image" not in request.files:
+        return "No image file provided", 400
+    image_file = request.files["image"]
+    image = Image.open(image_file)
+
+    # 画像をモノクロに変換する処理
+    monochrome_image = image.convert("L")
+
+    # モノクロ画像をバイナリデータに変換する
+    output = io.BytesIO()
+    monochrome_image.save(output, format="PNG")
+    output.seek(0)
+
+    return send_file(output, mimetype="image/png")
 
 
 if __name__ == "__main__":
